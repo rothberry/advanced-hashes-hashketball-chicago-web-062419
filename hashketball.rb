b84b8b4ed71dac943a987826cf194f43753693ed
Phil Roth <phil.roth077@gmail.com>
2019-06-13 19:08:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -124,22 +124,6 @@ def game_hash
   }
 end
 
-# def num_points_scored(player_name)
-#   game_hash.each do |location, team_data|
-#     team_data.each do |attribute, data|
-#       binding.pry
-#       if attribute == :players
-#         data.each do |x|
-#           if x[:player_name] == player_name
-#             binding.pry
-#             return x[:points]
-#           end
-#         end
-#       end
-#     end
-#   end
-# end
-
 def num_points_scored(player_name)
   game_hash.each do |team, players|
       players[:players].each do |x|
