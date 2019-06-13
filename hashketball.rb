d04f5093d2d3e3ec0837842bc0ab17302dc0996c
Phil Roth <phil.roth077@gmail.com>
2019-06-13 20:13:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -232,13 +232,9 @@ def winning_team
   home_points = 0
   away_points = 0
   game_hash.each do |team, details|
-      details[:players].each do |x|
-        if x[x.keys.join][:points] > most_points
-          most_points = x[x.keys.join][:points]
-          player = x.keys.join
-          #binding.pry
-        end
-      #binding.pry
+    binding.pry
+    details.[:players].each do |x|
+      binding.pry
     end
   end
 end
