7aa23e26c650a95ffd6fbb42aef0e322a0c0a8fb
Phil Roth <phil.roth077@gmail.com>
2019-06-13 19:09:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -136,4 +136,15 @@ def num_points_scored(player_name)
   end
 end
 
+def shoe_size(player_name)
+  game_hash.each do |team, players|
+      players[:players].each do |x|
+      #binding.pry
+      if x.has_key?(player_name)
+        return x[player_name][:shoe]
+      end
+      #binding.pry
+    end
+  end
+end
 num_points_scored("Jeff Adrien")
